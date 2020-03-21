60955dc9371767d90ccbff61a8334dd5b38a1b39
Abhash <abhash24oct@gmail.com>
2020-03-21 08:44:12 +0100

    Added the controllers closes #3

---
+++ src/main/java/abhash/springframework/web/service/BeerService.java
@@ -0,0 +1,17 @@
+package abhash.springframework.web.service;
+
+import java.util.UUID;
+
+import abhash.springframework.web.model.BeerDto;
+
+public interface BeerService {
+
+	BeerDto getBeerId(UUID beerId);
+
+	BeerDto updateBeer(UUID beerId);
+
+	BeerDto saveBeer(BeerDto beerDto);
+
+	BeerDto getBeerByUpc(int upc);
+
+}
