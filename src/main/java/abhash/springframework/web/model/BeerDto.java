2e459c75961734806e460642bc6fa10de4dc30cd
Abhash <abhash24oct@gmail.com>
2020-03-21 07:42:35 +0100

    Added model objects closes #2

---
+++ src/main/java/abhash/springframework/web/model/BeerDto.java
@@ -0,0 +1,29 @@
+package abhash.springframework.web.model;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+	
+	private String beerName;
+	private BeerStyleEnum beerStyle;
+	private String upc;
+	private int quantityOnHand;
+	private long price;
+	
+	private UUID uuid;
+	private int version;
+	
+	private OffsetDateTime createdDate;
+	private OffsetDateTime lastModifiedDate;
+
+}
